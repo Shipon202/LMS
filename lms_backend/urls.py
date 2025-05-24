a1ea2f3238e8e4ca0e440767ff325e6cd7c9fdf7
Shipon202 <rsshipon97@gmail.com>
2025-05-25 03:37:42 +0600

    Updated URLs, requirements, and notes

--- lms_backend/urls.py
+++ lms_backend/urls.py
@@ -17,6 +17,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+   openapi.Info(
+      title="LMS API",
+      default_version='v1',
+      description="Learning Management System API Documentation",
+      terms_of_service="https://www.yourterms.com/",
+      contact=openapi.Contact(email="contact@lms.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
+
+
+
+
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -33,5 +56,14 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+      # Swagger URLs
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
+    #     # Your existing token paths
+    # path('api/token/', include('your_token.urls')), 
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
