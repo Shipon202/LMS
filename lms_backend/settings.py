a2700b7af97b31afddbbc17cbd30ae5a88926224
Shipon202 <rsshipon97@gmail.com>
2025-05-27 06:08:36 +0600

    Fix: add Render domain to ALLOWED_HOSTS

--- lms_backend/settings.py
+++ lms_backend/settings.py
@@ -63,6 +63,7 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # MUST be first (or at least before CommonMiddleware)
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,8 +71,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'corsheaders.middleware.CorsMiddleware',
-      'django.middleware.common.CommonMiddleware',
 ]
 
 
