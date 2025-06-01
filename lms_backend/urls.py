6fe6b71ecf1eb857022a56245146ec1af5311a9a
Shipon202 <rsshipon97@gmail.com>
2025-06-02 00:11:16 +0600

    Describe what you changed

--- lms_backend/urls.py
+++ lms_backend/urls.py
@@ -52,6 +52,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('users.urls')),
+    
     path('api/', include('core.urls')),
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
